4f21aa7b1cb50d3dce54cb7162fd4afd17ecd62b
Bhumit Reddy Basireddy <bhumitreddybasireddy@gmail.com>
2026-05-19 19:41:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.15
+});
+
+fadeElements.forEach((element) => observer.observe(element));
